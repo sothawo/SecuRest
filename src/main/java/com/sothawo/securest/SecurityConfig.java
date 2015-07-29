4369f26e77f1825ee92aebcbbb04ec22a97fc033
Peter-Josef Meisch <pj.meisch@sothawo.com>
2015-07-29 17:28:02 +0200

    adding the missing HTTP verbs; disabled csrf

--- src/main/java/com/sothawo/securest/SecurityConfig.java
+++ src/main/java/com/sothawo/securest/SecurityConfig.java
@@ -7,6 +7,7 @@ package com.sothawo.securest;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
@@ -24,5 +25,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user1").password("secret1").roles("USER")
                 .and()
                 .withUser("user2").password("secret2").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated();
+        http.httpBasic();
+        http.csrf().disable();
     }
 }
--- src/main/java/com/sothawo/securest/UserService.java
+++ src/main/java/com/sothawo/securest/UserService.java
@@ -8,6 +8,7 @@ package com.sothawo.securest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +22,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/service")
 public class UserService {
 
-    @RequestMapping(value = "/echo/{in}", method = RequestMethod.GET)
+    @RequestMapping(value = "echo/{in}", method = RequestMethod.GET)
     public String echo(@PathVariable(value = "in") final String in, @AuthenticationPrincipal final UserDetails user) {
         return "Hello " + user.getUsername() + ", you said: " + in;
+    }
+
+    @RequestMapping(value = "post", method = RequestMethod.POST)
+    public void post(@RequestBody final String in, @AuthenticationPrincipal final UserDetails user) {
+        System.out.println("User " + user.getUsername() + " posting " + in);
+    }
+
+    @RequestMapping(value = "put", method = RequestMethod.PUT)
+    public void put(@RequestBody final String in, @AuthenticationPrincipal final UserDetails user) {
+        System.out.println("User " + user.getUsername() + " putting " + in);
+    }
+    @RequestMapping(value = "delete/{in}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "in") final String in, @AuthenticationPrincipal final UserDetails user) {
+        System.out.println("User " + user.getUsername() + " deleting " + in);
     }
 }
