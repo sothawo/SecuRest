106d58a3162167d0a1700ea6dc49e3a7e231c354
Peter-Josef Meisch <pj.meisch@sothawo.com>
2015-07-29 10:00:42 +0200

    code cleanup

--- src/main/java/com/sothawo/securest/UserService.java
+++ src/main/java/com/sothawo/securest/UserService.java
@@ -8,7 +8,6 @@ package com.sothawo.securest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,10 +24,5 @@ public class UserService {
     @RequestMapping(value = "/echo/{in}", method = RequestMethod.GET)
     public String echo(@PathVariable(value = "in") final String in, @AuthenticationPrincipal final UserDetails user) {
         return "Hello " + user.getUsername() + ", you said: " + in;
-    }
-
-    @RequestMapping(value = "post", method = RequestMethod.POST)
-    public void post(@RequestBody final String in) {
-        System.out.println("posting " + in);
     }
 }
